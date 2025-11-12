5a2d6656aca1009c30de15122149ee5977aed152
[dyad] <huulong111@gmail.com>
2025-11-12 18:41:25 +0700

    Reverted all changes back to version c2d637e4f3ba098df8e31a14219f650e4ab0d4b3

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { cn } from '@/lib/utils';
 
-const topNavItems = [
-  {
-    path: '/',
-    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gscw16ey_expires_30_days.png",
-    tooltip: "Trang chủ",
-  },
-  {
-    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/07w0lrq8_expires_30_days.png",
-    tooltip: "Icon 2",
-  },
-  {
-    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gp6enmol_expires_30_days.png",
-    tooltip: "Icon 3",
-  },
-  {
-    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/2z2zk51q_expires_30_days.png",
-    tooltip: "Icon 4",
-  },
-  {
-    path: '/settings',
-    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/pj4kofh0_expires_30_days.png",
-    tooltip: "Cài đặt",
-  },
+const topIcons = [
+  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gscw16ey_expires_30_days.png",
+  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/07w0lrq8_expires_30_days.png",
+  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gp6enmol_expires_30_days.png",
+  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/2z2zk51q_expires_30_days.png",
+  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/pj4kofh0_expires_30_days.png",
 ];
 
 const bottomIcons = [
@@ -38,8 +20,6 @@ const bottomIcons = [
 ];
 
 const Sidebar = () => {
-  const location = useLocation();
-
   return (
     <aside className="flex flex-col items-center w-8">
       <img
@@ -48,53 +28,31 @@ const Sidebar = () => {
         alt="divider"
       />
       <div className="flex flex-col items-center self-stretch flex-grow gap-3">
-        {topNavItems.map((item, index) => {
-          const isActive = item.path ? location.pathname === item.path : false;
-
-          const iconContent = (
-            <div className={cn(
-              "flex items-center justify-center w-8 h-8 rounded-md",
-              isActive && "bg-gray-100 border border-solid border-gray-200"
-            )}>
-              {isActive ? (
-                <div className="flex items-center justify-center w-6 h-6 bg-orange-500 rounded-md">
-                  <img 
-                    src={item.icon} 
-                    className="w-4 h-4 object-contain"
-                    alt={item.tooltip}
-                    style={{ filter: 'brightness(0) invert(1)' }}
-                  />
-                </div>
-              ) : (
-                <img 
-                  src={item.icon} 
-                  className="w-5 h-5 object-contain"
-                  alt={item.tooltip}
-                />
-              )}
-            </div>
-          );
-
-          if (item.path) {
+        {topIcons.map((src, index) => {
+          // The first icon links to home
+          if (index === 0) {
+            return (
+              <Link to="/" key={index}>
+                <img src={src} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />
+              </Link>
+            );
+          }
+          // The fifth icon (gear) links to settings
+          if (index === 4) {
             return (
               <Tooltip key={index}>
                 <TooltipTrigger asChild>
-                  <Link to={item.path}>
-                    {iconContent}
+                  <Link to="/settings">
+                    <img src={src} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />
                   </Link>
                 </TooltipTrigger>
                 <TooltipContent side="right" className="bg-orange-100 text-orange-800 border-orange-200">
-                  <p>{item.tooltip}</p>
+                  <p>Cài đặt</p>
                 </TooltipContent>
               </Tooltip>
             );
           }
-          
-          return (
-            <div key={index} className="flex items-center justify-center w-8 h-8">
-              <img src={item.icon} className="w-5 h-5 object-contain" alt={item.tooltip} />
-            </div>
-          );
+          return <img key={index} src={src} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />;
         })}
       </div>
       <div className="flex flex-col items-center self-stretch gap-3 mt-4">
