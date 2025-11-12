6a7ab4801f8e45473013325f3ab0dbb0d2193047
[dyad] <huulong111@gmail.com>
2025-11-12 18:27:08 +0700

    [dyad] Làm nổi bật biểu tượng sidebar đang hoạt động - wrote 1 file(s)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 
-const topIcons = [
-  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gscw16ey_expires_30_days.png",
-  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/07w0lrq8_expires_30_days.png",
-  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gp6enmol_expires_30_days.png",
-  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/2z2zk51q_expires_30_days.png",
-  "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/pj4kofh0_expires_30_days.png",
+const topNavItems = [
+  {
+    path: '/',
+    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gscw16ey_expires_30_days.png",
+    tooltip: "Trang chủ"
+  },
+  {
+    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/07w0lrq8_expires_30_days.png",
+  },
+  {
+    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/gp6enmol_expires_30_days.png",
+  },
+  {
+    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/2z2zk51q_expires_30_days.png",
+  },
+  {
+    path: '/settings',
+    icon: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wjyXx6yIud/pj4kofh0_expires_30_days.png",
+    tooltip: "Cài đặt"
+  }
 ];
 
 const bottomIcons = [
@@ -28,31 +43,39 @@ const Sidebar = () => {
         alt="divider"
       />
       <div className="flex flex-col items-center self-stretch flex-grow gap-3">
-        {topIcons.map((src, index) => {
-          // The first icon links to home
-          if (index === 0) {
-            return (
-              <Link to="/" key={index}>
-                <img src={src} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />
-              </Link>
-            );
+        {topNavItems.map((item, index) => {
+          if (!item.path) {
+            return <img key={index} src={item.icon} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />;
           }
-          // The fifth icon (gear) links to settings
-          if (index === 4) {
+
+          const navLink = (
+            <NavLink
+              to={item.path}
+              className={({ isActive }) =>
+                cn(
+                  "flex items-center justify-center w-8 h-8 rounded-lg",
+                  isActive && "bg-white border border-blue-200 shadow-sm"
+                )
+              }
+            >
+              <img src={item.icon} className="w-5 h-5 object-contain" alt={item.tooltip || `icon ${index + 1}`} />
+            </NavLink>
+          );
+
+          if (item.tooltip) {
             return (
               <Tooltip key={index}>
                 <TooltipTrigger asChild>
-                  <Link to="/settings">
-                    <img src={src} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />
-                  </Link>
+                  {navLink}
                 </TooltipTrigger>
                 <TooltipContent side="right" className="bg-orange-100 text-orange-800 border-orange-200">
-                  <p>Cài đặt</p>
+                  <p>{item.tooltip}</p>
                 </TooltipContent>
               </Tooltip>
             );
           }
-          return <img key={index} src={src} className="w-8 h-8 object-fill" alt={`icon ${index + 1}`} />;
+          
+          return navLink;
         })}
       </div>
       <div className="flex flex-col items-center self-stretch gap-3 mt-4">
