f1f4bb1b7658635f9ccf92b4944de5f27bb12aa0
[dyad] <huulong111@gmail.com>
2025-11-14 22:08:55 +0700

    [dyad] Sửa lỗi layout bị tràn màn hình - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,16 +4,14 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="bg-[#F6F8FA] min-h-screen flex flex-col">
-      <div className="w-full flex flex-col flex-1">
-        <Header />
-        <main className="flex flex-1 items-stretch my-3 mx-2 gap-2">
-          <Sidebar />
-          <div className="flex items-start bg-white flex-1 rounded-lg border border-solid border-[#EDEDED]">
-            <Outlet />
-          </div>
-        </main>
-      </div>
+    <div className="bg-[#F6F8FA] h-screen flex flex-col">
+      <Header />
+      <main className="flex flex-1 items-stretch p-2 gap-2 overflow-hidden">
+        <Sidebar />
+        <div className="flex bg-white flex-1 rounded-lg border border-solid border-[#EDEDED]">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 };
